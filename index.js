68b25ef945d894e3c9538402e6a6cdd753b10e2e
fnixdev <luisgatn000@gmail.com>
2021-12-21 12:35:17 -0300

    ++

--- index.js
+++ index.js
@@ -1,45 +1,133 @@
-// De 104 para 32
-const fs = require("fs")
-const { default: makeWASocket, BufferJSON, initInMemoryKeyStore, DisconnectReason, MessageType,
-    MessageOptions, MimeType, useSingleFileAuthState } = require("@adiwajshing/baileys-md")
-const { state, saveState } = useSingleFileAuthState('./config/shirosession.json')
-const config = require('./config/config.json')
-const { banner, getBuffer, getRandom } = require('./src/functions')
 
-// imports
+const {
+    MessageType,
+    WAMessage,
+    ReconnectMode,
+    WAProto,
+    useSingleFileAuthState,
+    MediaType,
+    DisconnectReason
+} = require('@adiwajshing/baileys-md')
+var pino = require("pino");
+var baileys = require("@adiwajshing/baileys-md");
+const axios = require('axios').default
+const fs = require('fs')
+const moment = require('moment-timezone')
+const chalk = require('chalk')
 
-const id = config.dev
-const prefix = config.prefix
+const color1 = (texto, cor) => {
+    return !color ? chalk.magenta(texto) : chalk.keyword(cor)(texto)
+}
+const color = (texto, cor) => {
+    return !color ? chalk.cyan(texto) : chalk.keyword(cor)(texto)
+}
+const getGroupAdmins = (participantes) => {
+    var admins = []
+    for (let i of participantes) {
+        i.admin === "admin" ? admins.push(i.id) : ''
+    }
+    return admins
+}
+const { saveState, state } = useSingleFileAuthState('./config/shirosession.json');
 
 
-const startSock = () => {
-    const sock = makeWASocket({ printQRInTerminal: true, auth: state })
-
-    sock.ev.on('messages.upsert', async m => {
-        const msg = m.messages[0]
-        if (!msg.key.fromMe && m.type === 'notify') {
-            console.log('+ respondendo: ', msg.key.remoteJid)
-            await sock.sendMessage(id,
-            { url: './src/shiro.jpeg' },
-            MessageType.image,
-            { mimetype: 'image/jpeg', caption: 'Opa! Shiro is alive' })
-        }
+(async () => {
+    const prefix = [
+        '!'
+    ]
+    const client = baileys["default"]({
+        printQRInTerminal: true,
+        browser: ['Shiro MD Session', "Safari", "3.0"],
+        logger: pino({ level: 'info' }),
+        auth: state
     })
 
-    sock.ev.on('connection.update', (update) => {
+
+    client.ev.on('messages.upsert', async m => {
+        try {
+            if (!msg.message) return
+            const msg = Object.keys(m.messages[0].message)[0].includes('senderKeyDistributionMessage') ? m.messages[1] : m.messages[0]
+            msg.message = (Object.keys(msg.message)[0] === 'ephemeralMessage') ? msg.message.ephemeralMessage.message : msg.message
+            
+            if (msg.key && msg.key.remoteJid == 'status@broadcast') return
+            if (msg.key.fromMe) return
+
+            const from = msg.key.remoteJid
+            const type = Object.keys(msg.message)[0]
+            const time = moment.tz('America/Sao_Paulo').format('HH:mm:ss')
+
+            var body = (type === 'conversation') ? msg.message.conversation : (type == 'imageMessage') ?
+                msg.message?.imageMessage?.caption : (type == 'videoMessage') ?
+                    msg.message?.videoMessage?.caption : (type == 'extendedTextMessage') ?
+                        msg.message?.extendedTextMessage?.text : (type == 'buttonsResponseMessage') ?
+                            msg.message.buttonsResponseMessage?.selectedButtonId : (type == 'liveLocationMessage') ?
+                                msg.message.liveLocationMessage?.caption : (type == 'listResponseMessage') ?
+                                    msg.message.listResponseMessage?.singleSelectReply?.selectedRowId : ''
+
+            const isMedia = type.includes('videoMessage') || type.includes('imageMessage') || type.includes('stickerMessage') || type.includes('audioMessage') || type.includes('documentMessage')
+
+            const isCmd = prefix.includes(body != '' && body.slice(0, 1)) && body.slice(1) != ''
+            const comando = isCmd ? body.slice(1).trim().split(' ')[0].toLowerCase() : ''
+            const args = body.trim().split(/ +/).slice(1)
+            const isGroup = from.endsWith('@g.us')
+            const author = isGroup ? msg.key.participant : msg.key.remoteJid
+            const pushname = msg.pushName || "No Name"
+
+            const groupMetadata = isGroup ? await client.groupMetadata(from) : ''
+            const groupName = isGroup ? groupMetadata.subject : ''
+            const groupMembers = isGroup ? groupMetadata.participants : ''
+            const groupAdmins = isGroup ? getGroupAdmins(groupMembers) : ''
+
+            const botNumber = client.user.id.includes(':') ? client.user.id.split(':')[0] + '@s.whatsapp.net' : client.user.id
+            const isGroupAdmins = groupAdmins.includes(author) || false
+            function isBotAdmin() {
+                var a1 = isGroup ? getGroupAdmins(groupMembers) : ''
+                if (a1.includes(botNumber)) return true
+                else return false
+            }
+
+            class conn {
+                static reply(message, mentions = [author], options = { quoted: msg }) {
+                    return client.sendMessage(from, { text: message, mentions: mentions }, options)
+                }
+                static sendImage(image, message = '', mentions = [author], options = { quoted: msg }) {
+                    return client.sendMessage(from, { image: image, mentions: mentions, caption: message }, options)
+                }
+            }
+            if (!isGroup && isCmd) console.log(color('COMMAND RECEIVED'), color(time, 'yellow'), color1(comando), 'OF', color1(pushname))
+            if (isCmd && isGroup) console.log(color('COMMAND RECEIVED'), color(time, 'yellow'), color1(comando), 'OF', color1(pushname), 'IN', color(groupName))
+
+            if (isCmd) {
+                switch (comando) {
+                    case 'msg':
+                        conn.reply(JSON.stringify(msg, null, '\t'))
+                        break
+                    case 'sendtext':
+                        conn.reply(`Text Here`)
+                        break
+                }
+            }
+
+        } catch (err) {
+            console.log(err)
+        }
+    })
+    client.ev.on('group-participants.update', async (update) => {
+        console.log(update)
+    })
+
+    client.ev.on('connection.update', (update) => {
         const { connection, lastDisconnect } = update
         if (connection === 'close') {
-            lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
-                ? startSock()
-                : console.log('+ connection closed')
+            //const shouldReconnect = (lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut
+            console.log('❌ - Connection Closed\nReason: ', lastDisconnect.error)
+            process.exit()
+        } else if (connection === 'open') {
+            console.log(color(`✅ - BOT Started!`, 'green'))
+        } else if (connection === 'connecting') {
+            console.log(color(`✅ - Connecting on WhatsApp Web...`, 'green'))
         }
-        console.log('+ connection update', update)
     })
-    
-    
-    sock.ev.on('creds.update', saveState)
 
-    return sock
-}
-
-startSock()
+   client.ev.on('creds.update', () => saveState)
+})()
