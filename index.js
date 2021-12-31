3084870044a596c23e8a6b24c111a4b30f0f51d0
fnixdev <luisgatn000@gmail.com>
2021-12-31 16:19:16 -0300

    @fnixdev

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ async function startShiro() {
                     ppgroup = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
                 }
                 if (anu.action == 'add') {
-                    shiro.sendMessage(anu.id, { image: { url: ppuser }, contextInfo: { mentionedJid: [num] }, caption: `Opa @${num.split("@")[0]}, bem vindo ao grupo ${metadata.subject}.\nEspero que tenha uma boa estadia.` })
+                    shiro.sendMessage(anu.id, { image: { url: ppuser }, contextInfo: { mentionedJid: [num] }, caption: `Opa @${num.split("@")[0]}, bem vindo ao grupo ${metadata.subject}. Espero que tenha uma boa estadia.` })
                 } else if (anu.action == 'remove') {
                     shiro.sendMessage(anu.id, { image: { url: ppuser }, contextInfo: { mentionedJid: [num] }, caption: `@${num.split("@")[0]} Saiu e foi joga no vasco.` })
                 }
