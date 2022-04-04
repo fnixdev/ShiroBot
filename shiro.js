29976e450b9b6157181e71a5f32256c9b360263c
fnixdev <luisgatn000@gmail.com>
2022-04-04 13:37:12 -0300

    +

--- shiro.js
+++ shiro.js
@@ -970,8 +970,8 @@ _Por enquanto não faço muita coisa_
 				let quality = args[1] ? args[1] : '128kbps'
 				let media = await yta(text, quality)
 				if (media.filesize >= 100000) return m.reply('File Melebihi Batas '+util.format(media))
-				hisoka.sendImage(m.chat, media.thumb, `⭔ Title : ${media.title}\n⭔ File Size : ${media.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP3\n⭔ Resolusi : ${args[1] || '128kbps'}`, m)
-				hisoka.sendMessage(m.chat, { audio: { url: media.dl_link }, mimetype: 'audio/mpeg', fileName: `${media.title}.mp3` }, { quoted: m })
+				shiro.sendImage(m.chat, media.thumb, `⭔ Title : ${media.title}\n⭔ File Size : ${media.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP3\n⭔ Resolusi : ${args[1] || '128kbps'}`, m)
+				shiro.sendMessage(m.chat, { audio: { url: media.dl_link }, mimetype: 'audio/mpeg', fileName: `${media.title}.mp3` }, { quoted: m })
 			}
 			break
 			case 'ytmp4':{
@@ -979,7 +979,7 @@ _Por enquanto não faço muita coisa_
 				let quality = args[1] ? args[1] : '360p'
 				let media = await ytv(text, quality)
 				if (media.filesize >= 100000) return m.reply('File Melebihi Batas '+util.format(media))
-				hisoka.sendMessage(m.chat, { video: { url: media.dl_link }, mimetype: 'video/mp4', fileName: `${media.title}.mp4`, caption: `⭔ Title : ${media.title}\n⭔ File Size : ${media.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP3\n⭔ Resolusi : ${args[1] || '360p'}` }, { quoted: m })
+				shiro.sendMessage(m.chat, { video: { url: media.dl_link }, mimetype: 'video/mp4', fileName: `${media.title}.mp4`, caption: `⭔ Title : ${media.title}\n⭔ File Size : ${media.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP3\n⭔ Resolusi : ${args[1] || '360p'}` }, { quoted: m })
 			}
 			break
 
