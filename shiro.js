28819dfd82ee573f8b9ed464511ff12d8b81856b
fnix <49885720+fnixdev@users.noreply.github.com>
2022-04-21 10:42:28 -0300

    Update shiro.js

--- shiro.js
+++ shiro.js
@@ -548,7 +548,7 @@ module.exports = shiro = async (shiro, m, chatUpdate, store) => {
       let btn = [{
         urlButton: {
           displayText: 'Baixar APK',
-          url: `https://cdn-124.anonfiles.com/LbP9FaWex2/c5114e00-1650143087/Minecraft-v1.18.12.01-xbox-servers.apk`
+          url: `https://anonfiles.com/pcueI0Y1xd/minecraft-1-18-30_apk`
         }
 			}]
       shiro.send5ButImg(m.chat, caption, shiro.user.name, link, btn)
